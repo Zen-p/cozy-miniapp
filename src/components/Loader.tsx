eed82a0113550f6d2fd0c9ca6f6f86ce4d9a27fe
evgenijpesko <peshkozena66@gmail.com>
2025-09-30 12:04:56 +0300

    bugfix:
    removed unused react imports

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 export default function Loader() {
   return <div className="w-8 h-8 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>;
 }
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { authenticateWithTelegram } from '../api/auth';
 import Loader from '../components/Loader';
 import ErrorMessage from '../components/ErrorMessage';
